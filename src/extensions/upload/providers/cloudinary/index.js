ddc78321bb08c705dfde59cec82ff77fcfa7a426
Jose Pablo Garcia <josepgar01@gmail.com>
2025-05-07 11:47:59 -0300

    cloudinary fixed 6

--- src/extensions/upload/providers/cloudinary/index.js
+++ src/extensions/upload/providers/cloudinary/index.js
@@ -4,37 +4,41 @@ const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
 
 module.exports = {
-  init(config) {
+  async upload(file, config) {
     cloudinary.config({
       cloud_name: config.cloud_name,
       api_key: config.api_key,
       api_secret: config.api_secret,
     });
 
-    return {
-      async upload(file) {
-        return new Promise((resolve, reject) => {
-          const uploadStream = cloudinary.uploader.upload_stream(
-            { resource_type: 'auto' },
-            (error, result) => {
-              if (error) return reject(error);
-              resolve({
-                url: result.secure_url,
-                provider_metadata: {
-                  public_id: result.public_id,
-                },
-              });
-            }
-          );
-          streamifier.createReadStream(file.buffer).pipe(uploadStream);
-        });
-      },
+    return new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { resource_type: 'auto' },
+        (err, result) => {
+          if (err) return reject(err);
 
-      async delete(file) {
-        const publicId = file.provider_metadata?.public_id;
-        if (!publicId) return;
-        return cloudinary.uploader.destroy(publicId);
-      },
-    };
+          resolve({
+            url: result.secure_url,
+            provider_metadata: {
+              public_id: result.public_id,
+            },
+          });
+        }
+      );
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
+    });
+  },
+
+  async delete(file, config) {
+    cloudinary.config({
+      cloud_name: config.cloud_name,
+      api_key: config.api_key,
+      api_secret: config.api_secret,
+    });
+
+    const publicId = file.provider_metadata?.public_id;
+    if (!publicId) return;
+
+    return cloudinary.uploader.destroy(publicId);
   },
 };
